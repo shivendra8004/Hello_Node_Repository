c444f45567b8e356e5d77017fbdeac39f5c7296e
SHIVENDRA8004 <jat13632@gmail.com>
2022-11-27 00:42:28 +0530

    Imported os module

--- modules.js
+++ modules.js
@@ -1,3 +1,6 @@
-const {people , ages} = require('./people');
+// const {people , ages} = require('./people');
 
-console.log(people,ages);
+// console.log(people,ages);
+
+const os = require('os'); // this imports the operating system module
+console.log(os.platform(),os.homedir()); // this prints the info about the os
