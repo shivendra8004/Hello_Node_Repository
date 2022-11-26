50f63ec9b1cccd2353ce1c6df9acc5be8c8075d3
SHIVENDRA8004 <jat13632@gmail.com>
2022-11-27 01:17:31 +0530

    Conditional Directory creation : only when directory is not exists

--- file.js
+++ file.js
@@ -3,13 +3,17 @@ const fs = require('fs'); // Fs stands for file system
 
 
 // Directories
+if(!fs.existsSync('./assets')){
 fs.mkdir('./assets',(err)=>{
     if(err){
         console.log("Error");
     }
     console.log("Folder Created");
 });
-
+}
+else{
+    console.log("Directory Exists");
+}
 
 
 // Deleting Files
