45d1997232a9ca467bb3935bf1bc43fd7b807995
SHIVENDRA8004 <jat13632@gmail.com>
2022-11-27 02:11:30 +0530

    Another method like last one

--- streams.js
+++ streams.js
@@ -2,9 +2,8 @@ const fs = require('fs');
 
 const readStream = fs.createReadStream("./docs/blog2.txt",{encoding: 'utf8'});
 const writeStream = fs.createWriteStream("./docs/blog3.txt");
-readStream.on("data",(chunk)=>{
-    console.log("-------New Chunk-------");
-    console.log(chunk);
-    writeStream.write("\nNew Chunk\n");
-    writeStream.write(chunk);
-})
+
+
+// Piping : it does same work as above function but more effectively and efficiently
+
+readStream.pipe(writeStream);
