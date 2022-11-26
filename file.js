96e625b53d1f77703f1b02a75ca61e26e7a942f8
SHIVENDRA8004 <jat13632@gmail.com>
2022-11-27 01:27:46 +0530

    In file system pplaying with directories is done

--- file.js
+++ file.js
@@ -1,19 +1,20 @@
-const fs = require('fs'); // Fs stands for file system
-
-
+const fs = require("fs"); // Fs stands for file system
 
 // Directories
-if(!fs.existsSync('./assets')){
-fs.mkdir('./assets',(err)=>{
-    if(err){
-        console.log("Error");
+if (!fs.existsSync("./assets")) {
+  fs.mkdir("./assets", (err) => {
+    if (err) {
+      console.log("Error");
     }
     console.log("Folder Created");
-});
+  });
+} else {
+  fs.rmdir("./assets", (err) => {
+    if (err) {
+      console.log("Error");
+    }
+    console.log("Folder deleted");
+  });
 }
-else{
-    console.log("Directory Exists");
-}
-
 
 // Deleting Files
