b68cd56c82bb6390d57de31766ece4cea41e20a8
SHIVENDRA8004 <jat13632@gmail.com>
2022-11-27 19:56:28 +0530

    Tut 3 ended about client and server side connection

--- modules.js
+++
@@ -1,6 +0,0 @@
-// const {people , ages} = require('./people');
-
-// console.log(people,ages);
-
-const os = require('os'); // this imports the operating system module
-console.log(os.platform(),os.homedir()); // this prints the info about the os
--- people.js
+++
@@ -1,7 +0,0 @@
-const people = ["Shiv","Raj","Kunal","Manav"];
-const ages = ["16","13","17","20","43"];
-console.log(people);
-
-module.exports = {
-    people , ages
-};
