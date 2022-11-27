af7d3407a2301862bd0b39dd1241f731538996de
SHIVENDRA8004 <jat13632@gmail.com>
2022-11-28 00:18:35 +0530

    Learned about the response object

--- server.js
+++ server.js
@@ -1,7 +1,13 @@
 const http = require('http');
 
 const server =  http.createServer((req, res)=>{
-    console.log("Request Made");
+    console.log(req.url+ "  "+ req.method);
+
+    // Set Header Content type
+    res.setHeader('Content-Type','text/html');
+    res.write("</p>Badhiya Yrr</p>");
+    res.write("</p>Nice Yrr</p>");
+    res.end();
 });
 
 server.listen(3000, 'localhost',()=>{
