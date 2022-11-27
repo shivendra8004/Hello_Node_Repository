41502d0e50c34d65e2484016d7733b094e4b50a3
SHIVENDRA8004 <jat13632@gmail.com>
2022-11-27 19:20:00 +0530

    Tut 3 Started

--- streams.js
+++
@@ -1,9 +0,0 @@
-const fs = require('fs');
-
-const readStream = fs.createReadStream("./docs/blog2.txt",{encoding: 'utf8'});
-const writeStream = fs.createWriteStream("./docs/blog3.txt");
-
-
-// Piping : it does same work as above function but more effectively and efficiently
-
-readStream.pipe(writeStream);
