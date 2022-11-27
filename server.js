acd05e898406b5c24102decd6f639d68cf042e01
SHIVENDRA8004 <jat13632@gmail.com>
2022-11-28 01:05:40 +0530

    Learned about routing multiple pages

--- server.js
+++ server.js
@@ -2,13 +2,29 @@ const http = require('http');
 const fs = require('fs');
 
 const server =  http.createServer((req, res)=>{
-    console.log(req.url+ "  "+ req.method);
+    console.log(req.url);
 
     //Set Header content type
     res.setHeader('Content-Type','text/html');
 
+    let path = './views/';
+
+    switch(req.url){
+        case '/':
+            path+='index.html';
+            break;
+
+        case '/about':
+            path+='about.html';
+            break;
+
+        default:
+            path +='404.html';
+            break;
+    }
+
     //send an HTML file
-    fs.readFile('./views/index.html',(err,data)=>{
+    fs.readFile(path,(err,data)=>{
         if(err){
             console.log(`There is a Error ${err}`);
         }
