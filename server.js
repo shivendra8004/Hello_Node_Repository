374d65909389afa6ed7fea20d5c427c2faf8c12b
SHIVENDRA8004 <jat13632@gmail.com>
2022-12-15 16:19:09 +0530

    Basic code

--- server.js
+++ server.js
@@ -1,60 +1,8 @@
-const http = require('http');
-const fs = require('fs');
-const _ = require('lodash');
-
-const server =  http.createServer((req, res)=>{
-    // Loadash
-    const num = _.random(0,20);
-    console.log(num);
-    const greet = _.once(()=>{
-        console.log("Hello Guys");
-    });
-    greet();
-    greet();
-
-
-    //Set Header content type
-    res.setHeader('Content-Type','text/html');
-
-    let path = './views/';
-
-    switch(req.url){
-        case '/':
-            path+='index.html';
-            res.statusCode = 200;
-            break;
-
-        case '/about':
-            path+='about.html';
-            res.statusCode = 200;
-            break;
-
-        case '/about-me':
-            res.statusCode = 301;
-            res.setHeader('Location','./about');
-            res.end();
-            break;
-
-        default:
-            path +='404.html';
-            res.statusCode = 404;
-            break;
-    }
-
-    //send an HTML file
-    fs.readFile(path,(err,data)=>{
-        if(err){
-            console.log(`There is a Error ${err}`);
-        }
-        else{
-            // res.write(data);
-            res.end(data);
-        }
-    });
-
-
+const http = require("http");
+const server = http.createServer((req,res)=>{
+    console.log(req.url , req.method);
+    res.write("<html><h1>Hello</h1></html>");
 });
-
-server.listen(3000, 'localhost',()=>{
-    console.log("Listening for request on porn no. 3000");
+server.listen(3000,'localhost',()=>{
+    console.log("Listening on port no. 3000");
 });
