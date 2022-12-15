ba6f3931924cfebb8a3d8cf8d0891dfb53fb1111
SHIVENDRA8004 <jat13632@gmail.com>
2022-12-15 16:32:01 +0530

    fsd exam

--- server.js
+++ server.js
@@ -1,8 +1,25 @@
 const http = require("http");
-const server = http.createServer((req,res)=>{
-    console.log(req.url , req.method);
-    res.write("<html><h1>Hello</h1></html>");
+const express = require('express');
+const app = express();
+app.use("/",(req,res,next)=>{
+    console.log("Started Basic middleware that always runs");
+    next();
 });
-server.listen(3000,'localhost',()=>{
+app.use("/about",(req,res)=>{
+    res.send("We are in about section");
+});
+
+app.listen(3000,'localhost',()=>{
     console.log("Listening on port no. 3000");
 });
+
+
+
+
+// const server = http.createServer((req,res)=>{
+//     console.log(req.url , req.method);
+//     res.write("<html><h1>Hello</h1></html>");
+// });
+// server.listen(3000,'localhost',()=>{
+//     console.log("Listening on port no. 3000");
+// });
