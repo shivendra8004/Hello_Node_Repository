1de0ef9756eeb66b02f1e88d36314e26b3009e2d
SHIVENDRA8004 <jat13632@gmail.com>
2022-12-28 10:30:18 +0530

    Lac1 revision

--- server.js
+++ server.js
@@ -1,25 +1,2 @@
-const http = require("http");
-const express = require('express');
-const app = express();
-app.use("/",(req,res,next)=>{
-    console.log("Started Basic middleware that always runs");
-    next();
-});
-app.use("/about",(req,res)=>{
-    res.send("We are in about section");
-});
-
-app.listen(3000,'localhost',()=>{
-    console.log("Listening on port no. 3000");
-});
-
-
-
-
-// const server = http.createServer((req,res)=>{
-//     console.log(req.url , req.method);
-//     res.write("<html><h1>Hello</h1></html>");
-// });
-// server.listen(3000,'localhost',()=>{
-//     console.log("Listening on port no. 3000");
-// });
+const name = "rio";
+console.log(name);
