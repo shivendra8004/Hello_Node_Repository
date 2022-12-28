75deffd1f90e30b728e221ef5627c0e15391e0ca
SHIVENDRA8004 <jat13632@gmail.com>
2022-12-28 11:12:45 +0530

    Lec 2 : Modules & Require Done

---
+++ modules.js
@@ -0,0 +1,7 @@
+const {name} = require("./test");
+
+console.log(name)
+// console.log(test.name);
+// console.log(test.Enr);
+// console.log(test.batch);
+// console.log(test.collage);
---
+++ test.js
@@ -0,0 +1,12 @@
+var name  = "Shivendra Jat";
+var Enr  = "Enr No. 201B256";
+var batch  = "Batch B8";
+var collage  = "JUET";
+console.log("Module Required successfully");
+
+module.exports = {
+    name,
+    Enr,
+    batch,
+    collage
+}
