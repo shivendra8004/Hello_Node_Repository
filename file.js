d50d0dcd7579113786fed4d43ffff7a7bec41ba1
SHIVENDRA8004 <jat13632@gmail.com>
2022-12-28 23:07:41 +0530

    Lec2: Fs Molude writefile Done

--- file.js
+++ file.js
@@ -1,12 +1,21 @@
-const fs = require('fs');
+const fs = require("fs");
 
 // Reading Files
 
-fs.readFile('./views/index.html',(err,data)=>{  //   Asynchronus function
-    if(err){
-        console.log(err);
-    }
-    console.log(data.toString());
-});
+// fs.readFile('./views/index.html',(err,data)=>{  //   Asynchronus function
+//     if(err){
+//         console.log(err);
+//     }
+//     console.log(data.toString());
+// });
 
-console.log("Last Line");
+// console.log("Last Line");
+
+// Writing the Files
+
+fs.writeFile("./views/test.txt", "Hello Shiva", (err) => {
+  if (err) {
+    console.log(err);
+  }
+  console.log("File Written Successfully");
+});
