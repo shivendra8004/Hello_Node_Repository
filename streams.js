6312f99359de322d820e48900581e422a1fd091f
SHIVENDRA8004 <jat13632@gmail.com>
2022-12-29 01:07:06 +0530

    Lec 2 done

--- streams.js
+++ streams.js
@@ -7,7 +7,10 @@ const writeStream = fs.createWriteStream("./views/index1.html", {
   encoding: "utf8",
 });
 
-readStream.on("data", (data) => {
-  writeStream.write("\n New Chunk Of Data \n");
-  writeStream.write(data);
-});
+// readStream.on("data", (data) => {
+//   writeStream.write("\n New Chunk Of Data \n");
+//   writeStream.write(data);
+// });
+
+// Pipeing   a sorter way to write data on another file
+readStream.pipe(writeStream);
