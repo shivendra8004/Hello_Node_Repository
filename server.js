7bb36f732c46098d2118d0f99381bcca7b6365a1
SHIVENDRA8004 <jat13632@gmail.com>
2022-12-30 23:33:57 +0530

    Lec 4 : request object done

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 const http = require("http");
 const server = http.createServer((req, res) => {
   console.log("Request Made");
+  console.log(req.url + " " + req.method);
 });
 
 server.listen(3000, "localhost", () => {
-  console.log("Listening on port no. 3000");
+  console.log("Listening on port no 3000");
 });
