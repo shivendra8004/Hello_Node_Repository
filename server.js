ce2d287784a5119a75455f3aab8b586fc6f57038
SHIVENDRA8004 <jat13632@gmail.com>
2022-12-30 23:46:26 +0530

    Lec 4 : setting header and Content Type is Done

--- server.js
+++ server.js
@@ -1,7 +1,12 @@
 const http = require("http");
 const server = http.createServer((req, res) => {
-  console.log("Request Made");
-  console.log(req.url + " " + req.method);
+  console.log(req.url);
+  console.log(req.method);
+  // Setting Header to Content Type
+
+  res.setHeader("Content-Type", "text/html");
+  res.write("<h1>Hello</h1>Hello Guys");
+  res.end();
 });
 
 server.listen(3000, "localhost", () => {
