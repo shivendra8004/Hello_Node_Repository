8cc13e483041120fe077eff41ae2ea41705c7639
SHIVENDRA8004 <jat13632@gmail.com>
2022-12-31 11:46:58 +0530

    Lec5 : LEarned about dependencies and lec5 done

--- server.js
+++ server.js
@@ -6,45 +6,45 @@ const server = http.createServer((req, res) => {
   const num = _.random(10, 30);
 
   res.setHeader("Content-Type", "text/html");
-  // let path = "./views/";
-  // switch (req.url) {
-  //   case "/":
-  //     path += "index.html";
-  //     res.statusCode = 200;
-  //     break;
-  //   case "/home":
-  //     res.setHeader("Location", "/");
-  //     res.statusCode = 301;
-  //     res.end();
-  //     break;
+  let path = "./views/";
+  switch (req.url) {
+    case "/":
+      path += "index.html";
+      res.statusCode = 200;
+      break;
+    case "/home":
+      res.setHeader("Location", "/");
+      res.statusCode = 301;
+      res.end();
+      break;
 
-  //   case "/help":
-  //     path += "help.html";
-  //     res.statusCode = 200;
-  //     break;
-  //   case "/helpMe":
-  //     res.statusCode = 301;
-  //     res.setHeader("Location", "/help");
-  //     res.end();
-  //     break;
+    case "/help":
+      path += "help.html";
+      res.statusCode = 200;
+      break;
+    case "/helpMe":
+      res.statusCode = 301;
+      res.setHeader("Location", "/help");
+      res.end();
+      break;
 
-  //   case "/about":
-  //     path += "about.html";
-  //     res.setHeader = 200;
-  //     break;
-  //   case "/aboutMe":
-  //     res.statusCode = 301;
-  //     res.setHeader("Location", "/about");
-  //     res.end();
-  //     break;
+    case "/about":
+      path += "about.html";
+      res.setHeader = 200;
+      break;
+    case "/aboutMe":
+      res.statusCode = 301;
+      res.setHeader("Location", "/about");
+      res.end();
+      break;
 
-  //   default:
-  //     path += "404.html";
-  //     res.statusCode = 404;
-  //     break;
-  // }
+    default:
+      path += "404.html";
+      res.statusCode = 404;
+      break;
+  }
 
-  fs.readFile("./views/index.html", (err, data) => {
+  fs.readFile(path, (err, data) => {
     if (err) {
       res.write(err);
       res.end();
