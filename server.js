a10f690e86394385ee7d2ad66a18b19e6b5c3ba9
SHIVENDRA8004 <jat13632@gmail.com>
2022-12-31 11:31:13 +0530

    Lec5: Require and used lodash module

--- server.js
+++ server.js
@@ -1,51 +1,60 @@
 const http = require("http");
 const fs = require("fs");
+const _ = require("lodash");
 
 const server = http.createServer((req, res) => {
+  const num = _.random(10, 30);
+
   res.setHeader("Content-Type", "text/html");
-  let path = "./views/";
-  switch (req.url) {
-    case "/":
-      path += "index.html";
-      res.statusCode = 200;
-      break;
-    case "/home":
-      res.setHeader("Location", "/");
-      res.statusCode = 301;
-      res.end();
-      break;
+  // let path = "./views/";
+  // switch (req.url) {
+  //   case "/":
+  //     path += "index.html";
+  //     res.statusCode = 200;
+  //     break;
+  //   case "/home":
+  //     res.setHeader("Location", "/");
+  //     res.statusCode = 301;
+  //     res.end();
+  //     break;
 
-    case "/help":
-      path += "help.html";
-      res.statusCode = 200;
-      break;
-    case "/helpMe":
-      res.statusCode = 301;
-      res.setHeader("Location", "/help");
-      res.end();
-      break;
+  //   case "/help":
+  //     path += "help.html";
+  //     res.statusCode = 200;
+  //     break;
+  //   case "/helpMe":
+  //     res.statusCode = 301;
+  //     res.setHeader("Location", "/help");
+  //     res.end();
+  //     break;
 
-    case "/about":
-      path += "about.html";
-      res.setHeader = 200;
-      break;
-    case "/aboutMe":
-      res.statusCode = 301;
-      res.setHeader("Location", "/about");
-      res.end();
-      break;
+  //   case "/about":
+  //     path += "about.html";
+  //     res.setHeader = 200;
+  //     break;
+  //   case "/aboutMe":
+  //     res.statusCode = 301;
+  //     res.setHeader("Location", "/about");
+  //     res.end();
+  //     break;
 
-    default:
-      path += "404.html";
-      res.statusCode = 404;
-      break;
-  }
+  //   default:
+  //     path += "404.html";
+  //     res.statusCode = 404;
+  //     break;
+  // }
 
-  fs.readFile(path, (err, data) => {
+  fs.readFile("./views/index.html", (err, data) => {
     if (err) {
       res.write(err);
       res.end();
     } else {
+      const greet = _.once(() => {
+        res.write("Hello");
+      });
+      greet();
+      greet();
+      res.write(num.toString());
       res.write(data);
       res.end();
       console.log("Working successfully on " + req.url + " url");
