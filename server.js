ceba526d31e0c9cc9e87e4fb321ab21e9ca7030f
SHIVENDRA8004 <jat13632@gmail.com>
2022-12-31 10:59:09 +0530

    Rivision Done Till Lect 4

---
+++ server.js
@@ -0,0 +1,57 @@
+const http = require("http");
+const fs = require("fs");
+
+const server = http.createServer((req, res) => {
+  res.setHeader("Content-Type", "text/html");
+  let path = "./views/";
+  switch (req.url) {
+    case "/":
+      path += "index.html";
+      res.statusCode = 200;
+      break;
+    case "/home":
+      res.setHeader("Location", "/");
+      res.statusCode = 301;
+      res.end();
+      break;
+
+    case "/help":
+      path += "help.html";
+      res.statusCode = 200;
+      break;
+    case "/helpMe":
+      res.statusCode = 301;
+      res.setHeader("Location", "/help");
+      res.end();
+      break;
+
+    case "/about":
+      path += "about.html";
+      res.setHeader = 200;
+      break;
+    case "/aboutME":
+      res.statusCode = 301;
+      res.setHeader("Location", "/about");
+      res.end();
+      break;
+
+    default:
+      path += "404.html";
+      res.statusCode = 404;
+      break;
+  }
+
+  fs.readFile(path, (err, data) => {
+    if (err) {
+      res.write(err);
+      res.end();
+    } else {
+      res.write(data);
+      res.end();
+      console.log("Working successfully on " + req.url + " url");
+    }
+  });
+});
+server.listen(3000, "localhost", () => {
+  console.log("Listening On Port No. 3000");
+});
