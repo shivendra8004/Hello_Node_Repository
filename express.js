53cb365c549e6d06ed3dde165e41dc56c51c754c
SHIVENDRA8004 <jat13632@gmail.com>
2023-01-02 11:08:06 +0530

    Lect6: Redirects done

--- express.js
+++ express.js
@@ -11,7 +11,14 @@ app.get("/about", (req, res) => {
   //   res.send("<p>About Page</p>");
   res.sendFile("./views/about.html", { root: __dirname });
 });
+app.get("/about-us", (req, res) => {
+  //   res.send("<p>About Page</p>");
+  res.redirect("./about");
+});
 app.get("/help", (req, res) => {
   //   res.send("<p>About Page</p>");
   res.sendFile("./views/help.html", { root: __dirname });
+});
+app.get("/help-me", (req, res) => {
+  res.redirect("./help");
 });
