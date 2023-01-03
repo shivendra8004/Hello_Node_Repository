04ab14ab84b59a6addd83b1e6b572390e5a5c2e1
SHIVENDRA8004 <jat13632@gmail.com>
2023-01-03 21:37:47 +0530

    Removed extra code

--- express.js
+++ express.js
@@ -4,19 +4,15 @@ app.listen(3000, () => {
   console.log("Listening On Port 3000");
 });
 app.get("/", (req, res) => {
-  //   res.send("<p>Home Page</p>");
   res.sendFile("./views/index.html", { root: __dirname });
 });
 app.get("/about", (req, res) => {
-  //   res.send("<p>About Page</p>");
   res.sendFile("./views/about.html", { root: __dirname });
 });
 app.get("/about-us", (req, res) => {
-  //   res.send("<p>About Page</p>");
   res.redirect("./about");
 });
 app.get("/help", (req, res) => {
-  //   res.send("<p>About Page</p>");
   res.sendFile("./views/help.html", { root: __dirname });
 });
 app.get("/help-me", (req, res) => {
