4d4fe4e66329f11a9a284f81bc662534bbeb8719
SHIVENDRA8004 <jat13632@gmail.com>
2023-01-20 23:15:36 +0530

    Revision

--- express.js
+++ express.js
@@ -1,23 +1,20 @@
 const express = require("express");
 const app = express();
 app.listen(3000, () => {
-  console.log("Listening On Port 3000");
+  console.log("Server Started");
 });
 app.get("/", (req, res) => {
   res.sendFile("./views/index.html", { root: __dirname });
 });
+app.get("/home", (req, res) => {
+  res.redirect("/");
+});
 app.get("/about", (req, res) => {
   res.sendFile("./views/about.html", { root: __dirname });
-});
-app.get("/about-us", (req, res) => {
-  res.redirect("./about");
 });
 app.get("/help", (req, res) => {
   res.sendFile("./views/help.html", { root: __dirname });
 });
-app.get("/help-me", (req, res) => {
-  res.redirect("./help");
-});
 app.use((req, res) => {
-  res.status(404).sendFile("./views/404.html", { root: __dirname });
+  res.sendFile("./views/404.html", { root: __dirname });
 });
