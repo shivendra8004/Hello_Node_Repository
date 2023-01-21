02dac26d99b5c2c939fed51a6ef10ad52780ae57
SHIVENDRA8004 <jat13632@gmail.com>
2023-01-21 16:57:50 +0530

    View Engines

--- express.js
+++ express.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 // Register View Engine
 app.set("view engine", "ejs");
-app.set("views", "views");
+// app.set("views", "views");
 app.listen(3000, () => {
   console.log("Server Started");
 });
