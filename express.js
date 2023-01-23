a3171cece6aa8803f003c637d0614b2eeb404ee7
SHIVENDRA8004 <jat13632@gmail.com>
2023-01-24 01:34:36 +0530

    MongoDB

--- express.js
+++ express.js
@@ -1,6 +1,7 @@
 const express = require("express");
-const app = express();
 const morgan = require("morgan");
+const mongoose = require("mongoose");
+const app = express();
 const dbURI =
   "mongodb+srv://shiva8004:shiva8004@nodejstut.baipdd6.mongodb.net/?retryWrites=true&w=majority";
 
