929adb0e4ea3f100bf275f77a8a5c60764da3ef6
SHIVENDRA8004 <jat13632@gmail.com>
2023-01-24 09:25:27 +0530

    Rivision Started

--- express.js
+++ express.js
@@ -1,48 +1,18 @@
 const express = require("express");
 const morgan = require("morgan");
-const mongoose = require("mongoose");
-const Blog = require("./models/blog");
 const app = express();
 
-// DataBase Connection String
-const dbURL =
-  "mongodb+srv://shiva8004:test@nodejstut.baipdd6.mongodb.net/node-tut?retryWrites=true&w=majority";
-mongoose
-  .connect(dbURL)
-  .then(() => {
-    console.log("Connection Done");
-    // Listen for Requests
-    app.listen(3000, () => {
-      console.log("Server Started");
-    });
-  })
-  .catch((err) => {
-    console.log(err);
-  });
 // Register View Engine
 app.set("view engine", "ejs");
 // app.set("views", "views");
+app.listen(3000, () => {
+  console.log("Listening Started");
+});
 
 // Middleware & Static Files
 app.use(express.static("public"));
 // Third Party Module
 app.use(morgan("tiny"));
-
-app.get("/add-blog", (req, res) => {
-  const blog = new Blog({
-    title: "Web Development",
-    snippet: "Lorem ipsum dolor sit, amet consectetur adipisicing elit.",
-    body: "lorem Lorem ipsum dolor sit, amet consectetur adipisicing elit. Lorem ipsum dolor sit, amet consectetur adipisicing elit.",
-  });
-  blog
-    .save()
-    .then((result) => {
-      res.send(result);
-    })
-    .catch((err) => {
-      console.log(err);
-    });
-});
 
 app.get("/", (req, res) => {
   const blogs = [
@@ -61,7 +31,6 @@ app.get("/", (req, res) => {
     },
   ];
   res.render("index", { title: "Home", blogs });
-  // res.sendFile("./views/index.html", { root: __dirname });
 });
 
 app.get("/home", (req, res) => {
@@ -74,15 +43,12 @@ app.get("/blogs", (req, res) => {
 
 app.get("/about", (req, res) => {
   res.render("about", { title: "About Us" });
-  // res.sendFile("./views/about.html", { root: __dirname });
 });
 
 app.get("/blogs/create", (req, res) => {
   res.render("newblog", { title: "Create Blog" });
-  // res.sendFile("./views/about.html", { root: __dirname });
 });
 
 app.use((req, res) => {
   res.status(404).render("404", { title: "Error 404" });
-  // res.status(404).sendFile("./views/404.html", { root: __dirname });
 });
